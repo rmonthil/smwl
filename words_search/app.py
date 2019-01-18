3b37148834111b67f0acb93fe0e6d391904e562d
Rémi MONTHILLER <remi.monthiller@etu.enseeiht.fr>
2019-01-18 14:17:12 +0100

    changed input and output directories

--- words_search/app.py
+++ words_search/app.py
@@ -8,15 +8,15 @@ from graph_to_tree_converter import *
 r = reader()
 g2t = graph_to_tree_converter()
 
-words = r.compute('input/dict.json')
-words, partitions = g2t.compute(words, 'output/ax_words.txt')
+words = r.compute('dict.json')
+words, partitions = g2t.compute(words, 'ax_words.txt')
 
-print("Storing result dictionary to : output/words.json")
+print("Storing result dictionary to : words.json")
 
-with open('output/words.json', 'w') as f:
+with open('words.json', 'w') as f:
     json.dump(words, f, sort_keys=True, indent=4)
 	
-print("Storing result dictionary to : output/parts.json")
+print("Storing result dictionary to : parts.json")
 	
-with open('output/parts.json', 'w') as f:
+with open('parts.json', 'w') as f:
     json.dump(partitions, f, sort_keys=True, indent=4)
